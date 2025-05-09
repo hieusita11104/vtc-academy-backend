e1bd20209ddb10deb29918f4ffdd8ffb60e2ce97
hieusita11104 <Duchieu111004@gmail.com>
2025-05-09 19:35:49 +0700

    Update index.js with new changes

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require("express");
+const { Sequelize } = require("sequelize");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-// router
+// Kết nối Sequelize
+const sequelize = new Sequelize("ecommerce", "root", "", {
+  host: "127.0.0.1",
+  dialect: "mysql",
+  port: 3306,
+  logging: false,
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connected successfully!");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+// Router
 const router = require("./routes/productRouter.js");
 const categoryRouter = require("./routes/categoryRouter.js");
-
 app.use("/api/products", router);
 app.use("/api/categories", categoryRouter);
 
